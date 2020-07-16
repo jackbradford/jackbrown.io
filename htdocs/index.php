c32cbe30bcee674691ecb28bdbb7fd3b1ea14643
Jack Brown <jack@jackbrown.io>
2020-07-16 11:28:48 -0700

    Added webpack files and composer.json.

---
+++ htdocs/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Jack Brown | jackbrown.io</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+        <link rel="icon" type="image/ico" href="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Jack Brown's personal home on the web.">
+        <meta name="author" content="Jack Brown">
+        <script src="https://kit.fontawesome.com/062254f9c8.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <section class="splash">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <div>
+                    <h1>Jack Brown</h1>
+                    <h2>Programmer &amp; Web Developer</h2>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a href=""><span class="far fa-envelope"></span></a>
+                            <a href="">Email</a>
+                        </li>
+                        <li>
+                            <a href=""><span class="fab fa-github"></span></a>
+                            <a href="">GitHub</a>
+                        </li>
+                        <li>
+                            <a href=""><span class="fab fa-linkedin-in"></span></a>
+                            <a href="">LinkedIn</a>
+                        </li>
+                        <li>
+                            <a href=""><span class="fab fa-instagram"></span></a>
+                            <a href="">Instagram</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
+
---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/jackbrown-io.jsx',
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+//    new HtmlWebpackPlugin({
+//      title: 'Production',
+//    }),
+  ],
+  output: {
+    filename: './jackbrown-io.bundle.js',
+    path: path.resolve(__dirname, 'htdocs/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
+
