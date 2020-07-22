6e920182fbca840565939ce0e21121f5be47b3e1
Jack Brown <jack@jackbrown.io>
2020-07-21 17:39:17 -0700

    Updated request entry point.
    The file now launches disphatch, my server-side router. That system is
    configured via disphatch.conf.json, which contains sensitive information
    and is not included in this repository.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,46 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Jack Brown | jackbrown.io</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
-        <link rel="icon" type="image/ico" href="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Jack Brown's personal home on the web.">
-        <meta name="author" content="Jack Brown">
-        <script src="https://kit.fontawesome.com/062254f9c8.js" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <section class="splash">
-            <div>
-                <img src="" alt="">
-            </div>
-            <div>
-                <div>
-                    <h1>Jack Brown</h1>
-                    <h2>Programmer &amp; Web Developer</h2>
-                </div>
-                <div>
-                    <ul>
-                        <li>
-                            <a href=""><span class="far fa-envelope"></span></a>
-                            <a href="">Email</a>
-                        </li>
-                        <li>
-                            <a href=""><span class="fab fa-github"></span></a>
-                            <a href="">GitHub</a>
-                        </li>
-                        <li>
-                            <a href=""><span class="fab fa-linkedin-in"></span></a>
-                            <a href="">LinkedIn</a>
-                        </li>
-                        <li>
-                            <a href=""><span class="fab fa-instagram"></span></a>
-                            <a href="">Instagram</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    </body>
-</html>
+<?php
+/**
+ * @file htdocs/index.php
+ * This file is the entry point for all requests.
+ *
+ */
+ini_set('error_reporting', E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/www/vhosts/jackbrown.io/error.log');
+
+require_once '/var/www/vhosts/jackbrown.io/vendor/autoload.php';
+require_once '/var/www/vhosts/jackbrown.io/src/Controllers/PublicController.php';
+
+use JackBradford\Disphatch\Router\Router;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$config = '/var/www/vhosts/jackbrown.io/disphatch.conf.json';
+$router = Router::init($config);
+$router->routeAndExecuteRequest();
+error_log('Done.');
 
