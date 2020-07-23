191c180aa289d9838b9a73d845917a7a8c90ccbb
Jack Brown <jack@jackbrown.io>
2020-07-22 22:35:59 -0700

    Added to home page styles for small-med phones.
    The page is still missing the main nav. The styles that exist, however,
    are made scalable by the use of em units.

--- src/component/home.js
+++ src/component/home.js
@@ -21,7 +21,37 @@ export default class Home extends Component {
                 </div>
             </section>
             <section className="home-intro">
-
+                <div className="featured-skill">
+                    <img src="/img/user-interface.svg" alt="A browser interface." />
+                    <div className="intro-container">
+                        <h2>Interfaces &amp; Integration</h2>
+                        <p>
+                            Back-end and front-end, web APIs and user experiences.
+                            I design them to work together.
+                        </p>
+                    </div>
+                </div>
+                <div className="featured-skill">
+                    <img src="/img/database.svg" alt="A database server." />
+                    <div className="intro-container">
+                        <h2>Database Development</h2>
+                        <p>
+                            I lay the burden of data integrity squarely on the
+                            database layer. My schemas accuately describe the
+                            systems they model.
+                        </p>
+                    </div>
+                </div>
+                <div className="featured-skill">
+                    <img src="/img/intuition.svg" alt="An eye, with an idea." />
+                    <div className="intro-container">
+                        <h2>Intuition, Modern Tech</h2>
+                        <p>
+                            I integrate both into my strategies. I see possibilities and use
+                            whatever tools are at hand to make the good ideas real.
+                        </p>
+                    </div>
+                </div>
             </section>
             </React.Fragment>
         );
